0910a4b7ea8d7e4e52aaa53d64b0813510039089
1312255201 <1312255201@qq.com>
2025-02-24 17:44:51 +0800

    [init] 创建基础项目

---
+++ gugufish-server-monitor-cilent/src/main/java/cn/gugufish/MonitorCilentApplication.java
@@ -0,0 +1,13 @@
+package cn.gugufish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorCilentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorCilentApplication.class, args);
+	}
+
+}
