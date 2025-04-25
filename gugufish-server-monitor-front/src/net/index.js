7b9fbc3771094e6d74b9771f604de5ddc4f66543
一只咕咕鱼 <1312255201@qq.com>
2025-04-25 09:39:30 +0800

    修正错误

--- gugufish-server-monitor-front/src/net/index.js
+++ gugufish-server-monitor-front/src/net/index.js
@@ -63,9 +63,9 @@ function internalPost(url, data, headers, success, failure, error = defaultError
         if(data.code === 200) {
             success(data.data)
         } else if(data.code === 401) {
-            failure('登录状态已过期，请重新登录！')
+            failure(data.message || '登录状态已过期，请重新登录！', data.code, url)
             deleteAccessToken(true)
-        } else {
+        }else {
             failure(data.message, data.code, url)
         }
     }).catch(err => error(err))
@@ -76,9 +76,9 @@ function internalGet(url, headers, success, failure, error = defaultError){
         if(data.code === 200) {
             success(data.data)
         } else if(data.code === 401) {
-            failure('登录状态已过期，请重新登录！')
+            failure(data.message || '登录状态已过期，请重新登录！', data.code, url)
             deleteAccessToken(true)
-        } else {
+        }else {
             failure(data.message, data.code, url)
         }
     }).catch(err => error(err))
